a49dc54e387e9348540242a9a01c393e34a546b5
Shihab <khanmd@msu.edu>
2023-03-06 16:38:01 -0500

    rcloen for google drive

--- sync.sh
+++ sync.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
 remote="khanmd@hpcc.msu.edu"
 rsync -azP "$1/" "$remote:~/$1"
+
+# rclone to copy from/to google drive, notice --max-age param
+# rclone copy -P --max-age 7d --no-traverse gdrive:reading/ reading/
